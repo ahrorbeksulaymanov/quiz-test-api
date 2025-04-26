65f2902e2a491730a6eecd26e193fc4ecb9a513a
ahrorbeksulaymanov <ahrorsulaymanov2@gmail.com>
2025-04-26 17:15:50 +0500

    auth is added

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AgeCategoryController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\OptionController;
@@ -14,14 +15,72 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::prefix('auth')->group(function () {
+    Route::post("/login", [AuthController::class, "login"]);
+    Route::post("/register", [AuthController::class, "register"]);
+    
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post("/logout", [AuthController::class, "logout"]);
+        Route::get("/me", [AuthController::class, "me"]);
+    });
+});
 
-// Route::get("question-test", [QuestionTestController::class, "index"]);
-Route::get('tests/{testId}/questions', [QuestionTestController::class, 'index']);
-Route::post('tests/{testId}/questions', [TestController::class, 'questionAttach']);
+// Route::post("/login", [AuthController::class, "login"]);
+// Route::post("/register", [AuthController::class, "register"]);
+// Route::middleware('auth:sanctum')->post("/logout", [AuthController::class, "logout"]);
+// Route::middleware('auth:sanctum')->get('/me', [AuthController::class, 'me']);
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('tests/{testId}/questions', [QuestionTestController::class, 'index']);
+    Route::post('tests/{testId}/questions', [TestController::class, 'questionAttach']);
+});
 Route::apiResource("categories", CategoryController::class);
-Route::apiResource("levels", LevelController::class);
-Route::apiResource("age-categories", AgeCategoryController::class);
-Route::apiResource("tests", TestController::class);
-Route::apiResource("questions", QuestionController::class);
-Route::apiResource("options", OptionController::class);
+
+$routes_array = [
+    'categories' => CategoryController::class,
+    'levels' => LevelController::class,
+    'age-categories' => AgeCategoryController::class,
+    'tests' => TestController::class,
+    'questions' => QuestionController::class,
+    'options' => OptionController::class,
+];
+
+Route::apiResources($routes_array, ['only' => ['index', 'show']]);
+
+Route::middleware('auth:sanctum')->group(function () use ($routes_array) {
+    Route::apiResources($routes_array, ['except' => ['index', 'show']]);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Route::apiResource("levels", LevelController::class);
+// Route::apiResource("age-categories", AgeCategoryController::class);
+// Route::apiResource("tests", TestController::class);
+// Route::apiResource("questions", QuestionController::class);
+// Route::apiResource("options", OptionController::class);
