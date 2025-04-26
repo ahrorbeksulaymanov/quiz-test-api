2fc9583a8ea3f3bf2b3583d8bee9db3c7c33d4de
ahrorbeksulaymanov <ahrorsulaymanov2@gmail.com>
2025-04-26 18:02:18 +0500

    some changes

--- routes/api.php
+++ routes/api.php
@@ -25,11 +25,6 @@ Route::prefix('auth')->group(function () {
     });
 });
 
-// Route::post("/login", [AuthController::class, "login"]);
-// Route::post("/register", [AuthController::class, "register"]);
-// Route::middleware('auth:sanctum')->post("/logout", [AuthController::class, "logout"]);
-// Route::middleware('auth:sanctum')->get('/me', [AuthController::class, 'me']);
-
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('tests/{testId}/questions', [QuestionTestController::class, 'index']);
     Route::post('tests/{testId}/questions', [TestController::class, 'questionAttach']);
